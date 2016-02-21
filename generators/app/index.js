0e77c41487a1babe29806378cda3b1273bdd3e94
Juan Scolari <juanscolari@gmail.com>
2016-02-21 13:07:25 -0300

    Change welcoming message

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,7 +9,7 @@ module.exports = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the doozie ' + chalk.red('generator-sys-angular') + ' generator!'
+      'Let\'s create an AngularJS application!'
     ));
 
     var prompts = [{
