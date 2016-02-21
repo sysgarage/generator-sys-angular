46211b6fc7e1d3e7e69dbbfbd10cf0667865a7d5
Juan Scolari <juanscolari@gmail.com>
2016-02-21 19:49:50 -0300

    Create <directive> subgenerator

---
+++ generators/directive/index.js
@@ -0,0 +1,51 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var _ = require('lodash');
+var filters = require('../../utils/filters.js');
+var convert = require('../../utils/convert.js');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function() {
+    var done = this.async();
+
+    var prompts = [{
+      type: 'input',
+      name: 'name',
+      message: 'Enter the directive name:',
+      default: 'sys-default'
+    }, {
+      type: 'input',
+      name: 'moduleName',
+      message: 'Enter the module name:',
+      default: 'app.directives.sysDefault',
+      filter: filters.moduleNameFilter
+    }];
+
+    this.prompt(prompts, function(props) {
+      this.props = props;
+      done();
+    }.bind(this));
+  },
+
+  writing: function() {
+    var folderName = convert.moduleToFolder(this.props.moduleName);
+    var destinationFolder = 'src/' + folderName;
+    var context = {
+      moduleName: this.props.moduleName,
+      directiveName: _.camelCase(this.props.name),
+      controllerName: _.upperFirst(_.camelCase(this.props.name)) + 'Controller',
+      templateUrl: folderName + _.kebabCase(this.props.name) + '.directive.html'
+    };
+
+    this.fs.copyTpl(
+      this.templatePath('directive.js'),
+      this.destinationPath(destinationFolder + _.kebabCase(this.props.name) + '.directive.js'),
+      context
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('directive.jade'),
+      this.destinationPath(destinationFolder + _.kebabCase(this.props.name) + '.directive.jade')
+    );
+  }
+});
---
+++ generators/directive/templates/directive.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('<%= moduleName %>')
+    .directive('<%= directiveName %>', <%= directiveName %>);
+
+  /* @ngInject */
+  function <%= directiveName %>() {
+    var directive = {
+      restrict: 'EA',
+      templateUrl: '<%= templateUrl %>',
+      scope: {},
+      link: link,
+      controller: <%= controllerName %>,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    function link(scope, el, attr, vm) {
+
+    }
+  }
+
+  /* @ngInject */
+  function <%= controllerName %>() {
+    var vm = this;
+  }
+})();
